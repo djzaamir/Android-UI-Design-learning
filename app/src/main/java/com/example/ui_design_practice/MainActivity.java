f90f03d3034a0ebc6c3105f4de40e755941d11df
Aamir <djzaamir@gmail.com>
2020-02-19 17:40:51 +0500

    Added Different color directives for different widgets and their different states in styles.xml

--- app/src/main/java/com/example/ui_design_practice/MainActivity.java
+++ app/src/main/java/com/example/ui_design_practice/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.ui_design_practice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Let the Screen stay on
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+
+        }else{
+
+        }
+    }
+
+
+    public void bringMeFood(View view) {
+        Toast.makeText(this, "Thankyou so much for your order\nWe wish you a good health.", Toast.LENGTH_LONG)
+             .show();
     }
 }
